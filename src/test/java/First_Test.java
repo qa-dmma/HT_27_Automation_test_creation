5b0148675a38b4209c9f17ddcbf1b8beae986f90
Dmitriy Marchenko <dmma.job@gmail.com>
2021-02-16 19:54:05 +0200

    Upload

--- src/test/java/First_Test.java
+++ src/test/java/First_Test.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,16 +9,31 @@ public class First_Test {
     private WebDriver driver;
 
     public static void main(String[] args) throws Exception {
-
+        System.setProperty("webdriver.chrome.driver","C:\\qa\\Chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
-        WebElement enter_button = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[20]/td/p/a/b"));
-        enter_button.click();
-        WebElement sing_up_button = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[3]/td/table/tbody/tr/td[4]/a"));
-        sing_up_button.click();
-        WebElement element2 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[3]/center/input[1]"));
-        element2.click();
-        Thread.sleep(5000);
-        driver.quit();
+        driver.get("https://www.sharelane.com/");
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[20]/td/p/a/b")).click();
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[3]/td/table/tbody/tr/td[4]/a")).click();
+        String sing_up_text = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[3]/td/p/b")).getText();
+        Assert.assertEquals("Sign Up", sing_up_text);
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td[2]/input")).sendKeys("61050");
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/input")).click();
+        Assert.assertEquals("Sign Up", sing_up_text);
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/input")).sendKeys("Dmitriy");
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td[2]/input")).sendKeys("Marchenko");
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/input")).sendKeys("qa@test.com");
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[4]/td[2]/input")).sendKeys("12345qwerty");
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[5]/td[2]/input")).sendKeys("12345qwerty");
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[6]/td[2]/input")).click();
+        WebElement notification_after_reg = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[4]/td/span"));
+        Assert.assertEquals("Account is created!", notification_after_reg.getText() );
+        String login_email = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[6]/td/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/b")).getText();
+        String login_pass = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[6]/td/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]")).getText();
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[1]/td/table/tbody/tr/td[1]/a/img")).click();
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[3]/td/table/tbody/tr/td[1]/p/input")).sendKeys(login_email);
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[3]/td/table/tbody/tr/td[2]/p/input")).sendKeys(login_pass);
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[3]/td/table/tbody/tr/td[3]/input")).click();
+
+
     }
 }
